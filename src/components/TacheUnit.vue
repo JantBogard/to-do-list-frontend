58799556561166c8b21d5221113bbb2899b90508
JantBogard <joancenkonga@gmail.com>
2023-12-19 13:03:27 +0100

    add delete action and favoris add/remove action

--- src/components/TacheUnit.vue
+++ src/components/TacheUnit.vue
@@ -7,23 +7,75 @@
       <h2 class="card-libelle-tache">{{ tache.libelle }}</h2>
       <p class="card-descrip-tache">{{ tache.description }}</p>
     </div>
-    <div class="card-favoris-tache">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
+    <div class="card-action">
+      <div
+        @click="changeFavoris"
+        class="card-button-tache"
+        :class="{ active: tache.isFavoris }"
       >
-        <path
-          d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z"
-          fill="currentColor"
-        />
-      </svg>
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z"
+            fill="currentColor"
+          />
+        </svg>
+      </div>
+      <div class="card-button-tache">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M3.25 22C3.25 21.5858 3.58579 21.25 4 21.25H20C20.4142 21.25 20.75 21.5858 20.75 22C20.75 22.4142 20.4142 22.75 20 22.75H4C3.58579 22.75 3.25 22.4142 3.25 22Z"
+            fill="currentColor"
+          />
+          <path
+            d="M11.5201 14.929L11.5201 14.9289L17.4368 9.01225C16.6315 8.6771 15.6777 8.12656 14.7757 7.22455C13.8736 6.32238 13.323 5.36846 12.9879 4.56312L7.07106 10.4799L7.07101 10.48C6.60932 10.9417 6.37846 11.1725 6.17992 11.4271C5.94571 11.7273 5.74491 12.0522 5.58107 12.396C5.44219 12.6874 5.33894 12.9972 5.13245 13.6167L4.04356 16.8833C3.94194 17.1882 4.02128 17.5243 4.2485 17.7515C4.47573 17.9787 4.81182 18.0581 5.11667 17.9564L8.38334 16.8676C9.00281 16.6611 9.31256 16.5578 9.60398 16.4189C9.94775 16.2551 10.2727 16.0543 10.5729 15.8201C10.8275 15.6215 11.0584 15.3907 11.5201 14.929Z"
+            fill="currentColor"
+          />
+          <path
+            d="M19.0786 7.37044C20.3071 6.14188 20.3071 4.14999 19.0786 2.92142C17.85 1.69286 15.8581 1.69286 14.6296 2.92142L13.9199 3.63105C13.9296 3.6604 13.9397 3.69015 13.9502 3.72028C14.2103 4.47 14.701 5.45281 15.6243 6.37602C16.5475 7.29923 17.5303 7.78999 18.28 8.05009C18.31 8.0605 18.3396 8.07054 18.3688 8.08021L19.0786 7.37044Z"
+            fill="currentColor"
+          />
+        </svg>
+      </div>
+      <div @click="deleteTache" class="card-button-tache">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M3 6.52381C3 6.12932 3.32671 5.80952 3.72973 5.80952H8.51787C8.52437 4.9683 8.61554 3.81504 9.45037 3.01668C10.1074 2.38839 11.0081 2 12 2C12.9919 2 13.8926 2.38839 14.5496 3.01668C15.3844 3.81504 15.4756 4.9683 15.4821 5.80952H20.2703C20.6733 5.80952 21 6.12932 21 6.52381C21 6.9183 20.6733 7.2381 20.2703 7.2381H3.72973C3.32671 7.2381 3 6.9183 3 6.52381Z"
+            fill="currentColor"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M11.5956 22H12.4044C15.1871 22 16.5785 22 17.4831 21.1141C18.3878 20.2281 18.4803 18.7749 18.6654 15.8685L18.9321 11.6806C19.0326 10.1036 19.0828 9.31511 18.6289 8.81545C18.1751 8.31579 17.4087 8.31579 15.876 8.31579H8.12404C6.59127 8.31579 5.82488 8.31579 5.37105 8.81545C4.91722 9.31511 4.96744 10.1036 5.06788 11.6806L5.33459 15.8685C5.5197 18.7749 5.61225 20.2281 6.51689 21.1141C7.42153 22 8.81289 22 11.5956 22ZM10.2463 12.1885C10.2051 11.7546 9.83753 11.4381 9.42537 11.4815C9.01321 11.5249 8.71251 11.9117 8.75372 12.3456L9.25372 17.6087C9.29494 18.0426 9.66247 18.3591 10.0746 18.3157C10.4868 18.2724 10.7875 17.8855 10.7463 17.4516L10.2463 12.1885ZM14.5746 11.4815C14.9868 11.5249 15.2875 11.9117 15.2463 12.3456L14.7463 17.6087C14.7051 18.0426 14.3375 18.3591 13.9254 18.3157C13.5132 18.2724 13.2125 17.8855 13.2537 17.4516L13.7537 12.1885C13.7949 11.7546 14.1625 11.4381 14.5746 11.4815Z"
+            fill="currentColor"
+          />
+        </svg>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import axios from "axios";
 import Tache from "../models/tache";
 import { defineProps } from "vue";
 
@@ -31,6 +83,23 @@ import { defineProps } from "vue";
 const props = defineProps({
   tache: Tache,
 });
+
+const changeFavoris = async () => {
+  const response = await axios.get(
+    `http://localhost:8000/tache/favoris/${props.tache.id}`
+  );
+
+  // eslint-disable-next-line vue/no-mutating-props
+  props.tache.isFavoris = response.data.isFavoris;
+};
+
+const deleteTache = async () => {
+  const response = await axios.delete(
+    `http://localhost:8000/tache/${props.tache.id}`
+  );
+
+  alert(response.data);
+};
 </script>
 
 <style scoped>
@@ -61,13 +130,12 @@ const props = defineProps({
   font-weight: 500;
 }
 
-.card-favoris-tache {
-  margin-left: auto;
+.card-button-tache {
   cursor: pointer;
   transition: all 0.5s;
 }
 
-.card-favoris-tache:hover,
+.card-button-tache:hover,
 .active {
   color: #448aff;
 }
@@ -75,5 +143,12 @@ const props = defineProps({
 .card-valid-tache input {
   width: 20px;
   height: 20px;
+}
+
+.card-action {
+  margin-left: auto;
+  display: flex;
+  align-items: center;
+  gap: 8px;
 }
 </style>
